804e02a463720079877fe54d86ffd61a6f4f45dd
YeVlad <150618209+YeVlad@users.noreply.github.com>
2024-09-20 16:52:05 +0200

    small up 2

--- src/pages/EventsBoardPage/EventsBoardPage.jsx
+++ src/pages/EventsBoardPage/EventsBoardPage.jsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
 import EventsList from "../../components/EventsList/EventsList";
 import NameOfThePage from "../../components/NameOfThePage/NameOfThePage";
+import axios from "axios";
 
 const EventsBoardPage = () => {
+  useEffect(() => {
+    async function fetchEvents() {
+      const response = await axios.get(
+        "https://event-board-b.onrender.com/events"
+      );
+      console.log(response);
+    }
+    fetchEvents();
+  }, []);
   return (
     <>
       <NameOfThePage />
